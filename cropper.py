cc7b3a3fbdc6f19662ed1cb79d74a2fa9e07e2fc
Takumi <takumi.ando826@gmail.com>
2019-11-09 16:00:13 +0900

    first commit

---
+++ cropper.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+import argparse
+
+
+def main(args):
+    root = Path(args.input)
+    if root.is_file():
+        convert(str(root))
+    elif root.is_dir():
+        for ext in [".jpg", ".JPG", ".png", ".PNG"]:
+            for path in sorted(list(root.glob(f"*{ext}"))):
+                convert(str(path))
+
+
+def convert(path):
+    img = plt.imread(path)
+    fig = plt.figure(dpi=200)
+    ax = fig.add_subplot(111)
+    ax.imshow(img)
+    plots_input = plt.ginput(n=4, mouse_add=1)
+    plots_input = np.array(plots_input, dtype=np.float32)
+    plt.close(fig)
+
+    left = np.argsort(plots_input[:, 0], axis=0)[0:2]
+    top, bottom = np.argsort(plots_input[left, 1])
+    topleft = plots_input[left][top]
+    bottomleft = plots_input[left][bottom]
+
+    right = np.argsort(plots_input[:, 0], axis=0)[2:4]
+    top, bottom = np.argsort(plots_input[right, 1])
+    topright = plots_input[right][top]
+    bottomright = plots_input[right][bottom]
+
+    plots_from = np.array([topleft, bottomleft, bottomright, topright])
+
+    h, w = (args.height, args.width)
+    plots_to = np.array([(0, 0),
+                         (0, h),
+                         (w, h),
+                         (w, 0)], dtype=np.float32)
+    mat = cv2.getPerspectiveTransform(plots_from, plots_to)
+    warped = cv2.warpPerspective(img, mat, (w, h))
+    warped_bgr = cv2.cvtColor(warped, cv2.COLOR_RGB2BGR)
+    save_as = f"{args.out/Path(path).stem}_cropped{Path(path).suffix}"
+    cv2.imwrite(str(save_as), warped_bgr)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input")
+    parser.add_argument("--out", default=".", type=Path)
+    parser.add_argument("--width", default=1600)
+    parser.add_argument("--height", default=900)
+    args = parser.parse_args()
+    main(args)
